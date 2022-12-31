72cebd1f75a8115634770f620357bd5430fadbd4
Shashank  Pandey <shashankpandey2001@gmail.com>
2022-12-31 07:49:06 +0530

    third commit

--- src/index.js
+++ src/index.js
@@ -4,35 +4,43 @@ import ReactDom from "react-dom";
 //CSS
 import "./index.css";
 
+//setup vars
+const firstBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/W/WEBP_402378-T2/images/I/717wykgZcBL._AC_UL900_SR900,600_.jpg",
+  title: "The January 6th Report",
+  author: "Shashank Pandey",
+};
+
+const secondBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/W/WEBP_402378-T2/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg",
+  title: "Atomic Habits",
+  author: "James Clear",
+};
+
 function Booklist() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 }
 
-const Book = () => {
-  const title = "The Bear and The Fern";
+const Book = (props) => {
+  console.log(props);
   return (
     <article className="book">
-      <img
-        src="https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/51nvk2Smg3L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg"
-        alt=" "
-      />
-      <h1>{title}</h1>
-      <h5>Shashank Pandey</h5>
+      <img src={props.img} alt=" " />
+      <h1>{props.title.toUpperCase()}</h1>
+      <h5>{props.author}</h5>
     </article>
   );
 };
